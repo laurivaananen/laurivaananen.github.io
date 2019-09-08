a8bdee873ac5a3a9c4bc9e8b3013ed4d0a1befeb
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2019-09-08 20:47:08 +0200

    Updates

---
+++ precache-manifest.db7207acced52663783b888ad99d7032.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ef50bc64732c4f1f06207b3c93507a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8762de45c6c875130be2",
+    "url": "/static/css/main.1dbfb568.chunk.css"
+  },
+  {
+    "revision": "2b8cac55fde8683b9ad7",
+    "url": "/static/js/2.90ce3622.chunk.js"
+  },
+  {
+    "revision": "8762de45c6c875130be2",
+    "url": "/static/js/main.20ba5da1.chunk.js"
+  },
+  {
+    "revision": "e0cb57d0c6add5b1ed0c",
+    "url": "/static/js/runtime~main.6fa6e5b9.js"
+  }
+]);
